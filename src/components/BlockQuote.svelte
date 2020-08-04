028fd589356c15377261307702ced041592eeb92
Matt Derocher <mjamesderocher@gmail.com>
2020-08-04 08:08:30 -0400

    Tracked BQ component

---
+++ src/components/BlockQuote.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+  import BlockContent from "@movingbrands/svelte-portable-text";
+  import serializers from "./serializers";
+</script>
+
+<script>
+  export let author;
+  export let text;
+</script>
+
+
+<blockquote>
+  <BlockContent blocks={text} {serializers} />
+---{author.givenNames.en.join(' ')} {author.familyName.en}
+</blockquote>

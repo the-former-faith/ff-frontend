352c3f1e66858ee0d03271ac23ed0b33b7089048
Matt Derocher <mjamesderocher@gmail.com>
2021-02-18 07:18:42 -0500

    Added documentlist

---
+++ src/components/DocumentList.svelte
@@ -0,0 +1,27 @@
+<script>
+  import ImageObject from './ImageObject.svelte'
+  import MetaAuthors from './MetaAuthors.svelte'
+
+  export let posts
+</script>
+
+<ul class="posts-list">
+  {#each posts as post}
+    <li>
+      <h3>
+        <a rel="prefetch" href="en/post/{post.slug.en.current}">{post.title.en}</a>
+      </h3>
+      {#if post.mainImage}
+        <ImageObject image={post.mainImage.file} ratio={{ x: 4, y: 3 }} />
+      {/if}
+      <div class="post-meta">
+        {#if post.authors}
+          <p>
+            <MetaAuthors authors={post.authors} />
+          </p>
+        {/if}
+        <p>{new Date(post._createdAt).toDateString()}</p>
+      </div>
+    </li>
+  {/each}
+</ul>

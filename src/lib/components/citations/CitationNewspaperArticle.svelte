4c3bf73781dcb2e8f344ba722fb2aacb65c81df8
Matt Derocher <mjamesderocher@gmail.com>
2021-07-13 08:08:25 -0400

    Fixed links in citations

--- src/lib/components/citations/CitationNewspaperArticle.svelte
+++ src/lib/components/citations/CitationNewspaperArticle.svelte
@@ -1,10 +1,11 @@
 <script>
   import MetaAuthors from '$lib/components/layout/MetaAuthors.svelte'
+  import { base } from '$app/paths'
   export let source
 </script>
 
 <p>
-  <a href="en/newspaper-article/{source.slug.en.current}"><strong>{source.title.en}</strong></a>
+  <a href="{base}/en/newspaper-article/{source.slug.en.current}"><strong>{source.title.en}</strong></a>
   {#if source.authors} by <MetaAuthors authors={source.authors} />{/if}
   {#if source.parent} in <em>{source.parent.title.en}</em>{/if}
   {#if source.pageStart}, page {source.pageStart}{/if}

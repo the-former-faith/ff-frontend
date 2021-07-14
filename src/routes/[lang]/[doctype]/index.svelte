ec1fcc59d7f269d512f501ad07f06d8fd745fd12
Matt Derocher <mjamesderocher@gmail.com>
2021-07-14 11:44:34 -0400

    Added basic pagination

--- src/routes/[lang]/[doctype]/index.svelte
+++ src/routes/[lang]/[doctype]/index.svelte
@@ -2,9 +2,7 @@
   import client from '$lib/scripts/sanityClient'
   import { convertToCamelCase, convertToTitleCase } from '$lib/scripts/utilities'
   import groq from 'groq'
-
-  import { goto, invalidate, prefetch, prefetchRoutes } from '$app/navigation';
-  import { getStores, navigating, page, session } from '$app/stores';
+  import { page } from '$app/stores';
 
   export async function load({ page }) {
     const { doctype } = page.params
@@ -29,7 +27,7 @@
         slug,
         file,
         mainImage->
-      } | order(_createdAt desc)[0...50],
+      } | order(_createdAt desc)[${currentPage - 1}...${currentPage * 50}],
       "count": count(*[_type == $doctypeCamelCase])
     }`
 
@@ -41,16 +39,12 @@
 
 <script>
   import DocumentList from '$lib/components/layout/DocumentList.svelte'
-  import { get } from 'svelte/store';
   export let docs
   export let doctype
   export let currentPage
 
-  const setParam = () => {
-    console.log('clicked')
-    $page.query.set("page","1")
-    goto(`${$page.path}?page=1`, { replaceState: true, noscroll: true })
-  }
+  const pages = Math.ceil(docs.count / 50)
+
 </script>
 
 <svelte:head>
@@ -59,10 +53,14 @@
 
 <h2>{convertToTitleCase(doctype, '-')} Archives</h2>
 
-<DocumentList docs={docs.list} />
-
-<button on:click={()=> setParam()}>Goto</button>
-<button on:click={()=> $page.query.set("page","0")}>Set query param</button>
-<button on:click={()=> console.log($page.query.get('page'))}>print weather to console</button>
-
 <p>Total Results: {docs.count}</p>
+
+{#if pages > 1}
+  <ul>
+    {#each Array(pages) as _, i}
+      <li><a href="{$page.path}?page={i + 1}">{i + 1}</a></li>
+    {/each}
+  </ul>
+{/if}
+
+<DocumentList docs={docs.list} />
